20e456e2aba7ef9c01db6b9da0b92a0e6ef6fc45
BgAndreasen <bir_and@hotmail.com>
2021-11-07 12:55:55 +0000

    added package

--- testing_tacont.R
+++ testing_tacont.R
@@ -1,4 +1,5 @@
 #testing tacont() function
+library(tidyverse)
 
 df <- read_csv("data/demo_dataset.csv", col_types = cols())
 source("taContFunctions.R", encoding = "utf-8")
@@ -7,4 +8,3 @@ ylab <- df$contaminant %>% unique()
 results <- taCont(df$sampling_year, y = df$measval_num_calc, censored = df$measval_cen_calc)
 
 results[[1]] + ylab(ylab)
-#ggsave("images/demoplot.png", width = 4, height = 6)
